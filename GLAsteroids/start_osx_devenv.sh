9f51423a85aa02811bb4bc421b7a0d66ed7d7e81
Michael Lombardi <miclomba@gmail.com>
2020-07-04 18:44:06 -0500

    create start_osx_devenv.sh for GLAsteroids project

---
+++ GLAsteroids/start_osx_devenv.sh
@@ -0,0 +1,30 @@
+# To run cmake we need command line tools for xcode:
+# install via:
+#     xcode-select --install
+
+export REPOS="$HOME/source/repos"
+export GLASTEROIDS_DIR="$REPOS/GLAsteroids/GLAsteroids"
+export X_DIR="$REPOS/Backend/X"
+
+export INSTALL_PATH="$GLASTEROIDS_DIR/out/xcode/install"
+export GLASTEROIDS_LINK_DIR="$GLASTEROIDS_DIR/out/xcode"
+export GLASTEROIDS_INCL_DIR="$GLASTEROIDS_DIR"
+
+# compile boost yourself
+export BOOST_INCL_DIR="$REPOS/boost/boost_1_69_0"
+export BOOST_LINK_DIR="$REPOS/boost/boost_1_69_0/stage/lib"
+
+# brew install freeglut
+export FREEGLUT_INCL_DIR="/usr/local/Cellar/freeglut/3.2.1/include"
+export FREEGLUT_LINK_LIB="/usr/local/Cellar/freeglut/3.2.1/lib"
+
+export X_LINK_DIR="$X_DIR/out/xcode/install"
+export X_INCL_DIR="$X_DIR"
+
+export PATH="$PATH:$X_LINK_DIR:$FREEGLUT_LINK_LIB"
+
+cmake -G Xcode ../../
+
+# to install run
+# 
+#    cmake --install out/xcode --config Debug
