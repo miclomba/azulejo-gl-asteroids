a75928ecfb90d7ed1696eff1741808914f78fc97
Michael Lombardi <miclomba@gmail.com>
2020-07-05 02:59:59 -0500

    cleanup GLAsteroids startup script

--- GLAsteroids/start_osx_devenv.sh
+++ GLAsteroids/start_osx_devenv.sh
@@ -3,12 +3,6 @@
 #     xcode-select --install
 
 export REPOS="$HOME/source/repos"
-export GLASTEROIDS_DIR="$REPOS/GLAsteroids/GLAsteroids"
-export X_DIR="$REPOS/Backend/X"
-
-export INSTALL_PATH="$GLASTEROIDS_DIR/out/xcode/install"
-export GLASTEROIDS_LINK_DIR="$GLASTEROIDS_DIR/out/xcode"
-export GLASTEROIDS_INCL_DIR="$GLASTEROIDS_DIR"
 
 # compile boost yourself
 export BOOST_INCL_DIR="$REPOS/boost/boost_1_69_0"
@@ -16,11 +10,9 @@ export BOOST_LINK_DIR="$REPOS/boost/boost_1_69_0/stage/lib"
 
 # brew install freeglut
 export FREEGLUT_INCL_DIR="/System/Library/Frameworks/GLUT.framework/Headers"
-export FREEGLUT_LINK_LIB=""
-export FREEGLUT_LINK_BIN=""
 
-export X_LINK_DIR="$X_DIR/out/xcode/install"
-export X_INCL_DIR="$X_DIR"
+export X_LINK_DIR="/usr/local/lib"
+export X_INCL_DIR="$REPOS/Backend/X"
 
 export PATH="$PATH:$X_LINK_DIR"
 
